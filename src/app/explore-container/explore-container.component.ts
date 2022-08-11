b555b49d768568daf47f36ff5e75d1ad560dbe96
Jalir Durán Nogales <jayniw@gmail.com>
2022-08-10 22:56:59 -0400

    funcionalidad implementada

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { interval } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
+
+import * as playAlert from 'alert-sound-notify';
+
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() finishDateString: string;
+  @Input() main: boolean;
+  @Input() gameTime: number;
+  mins = 0;
+  segs = 0;
   time!: {
     days: number;
     hours: number;
     minutes: number;
     seconds: number;
   };
+  startDate: Date;
   finishDate: Date = new Date();
+  counterTimer$;
+  runnig = false;
+  paused = false;
 
-  constructor() {}
+  constructor(private alertController: AlertController) {
+    playAlert.content.fail = ['../assets/fail.mp3'];
+    playAlert.content.ring = ['../assets/ring.mp3'];
+    playAlert.content.minute = ['../assets/minute.mp3'];
+  }
 
   ngOnInit(): void {
     // Inicializamos el momento que falta hasta llegaral tiempo objetivo con valores en 0
@@ -26,13 +41,21 @@ export class ExploreContainerComponent implements OnInit {
       minutes: 0,
       seconds: 0,
     };
-    // Creamos la fecha a partir de la fecha en formato string AAAA-MM-dd HH:mm:ss
-    //this.finishDate = new Date(this.finishDateString);
-
-    //this.start().subscribe((_) => console.log('tik'));
+    this.mins = this.gameTime;
+    //asignamos un tiempo por defecto si no se paso el dato
+    if (this.gameTime === null || this.gameTime <= 0) {
+      this.mins = 50;
+      this.segs = 0;
+    }
+    // Creamos la fecha a partir de los minutos
+    this.setFinishDate(this.mins, this.segs);
     this.updateTime();
   }
 
+  /**
+   * It calculates the difference between the current date and the finish date, and then it assigns the
+   * difference to the time object
+   */
   updateTime() {
     const now = new Date();
     const diff = this.finishDate.getTime() - now.getTime();
@@ -50,8 +73,14 @@ export class ExploreContainerComponent implements OnInit {
     this.time.seconds = secs - mins * 60;
   }
 
-  // Ejecutamos la acción cada segundo, para obtener la diferencia entre el momento actual y el objetivo
+  /**
+   * It returns an observable that emits a number every second, and updates the time
+   *
+   * @returns An Observable that emits a number every second.
+   */
   start() {
+    this.startDate = new Date();
+    this.runnig = true;
     return interval(1000).pipe(
       map((x: number) => {
         this.updateTime();
@@ -60,19 +89,62 @@ export class ExploreContainerComponent implements OnInit {
     );
   }
 
-  inciar() {
+  /**
+   * Set the finishDate property to a new Date object, and set the minutes to the current minutes plus
+   * the minutes parameter.
+   *
+   * @param mins - number - the number of minutes to add to the current time to set the finish
+   * date
+   */
+  setFinishDate(mins: number, segs: number): void {
     this.finishDate = new Date();
-    this.finishDate.setMinutes(this.finishDate.getMinutes() + 50);
-    const counterTimer$ = this.start().subscribe((_) => {
-      if (this.time.days <= 0) {
-        this.time = {
-          hours: 0,
-          minutes: 0,
-          seconds: 0,
-          days: 0,
-        };
-        counterTimer$.unsubscribe();
+    this.finishDate.setMinutes(this.finishDate.getMinutes() + mins);
+    this.finishDate.setSeconds(this.finishDate.getSeconds() + segs);
+  }
+
+  play() {
+    this.runnig = true;
+    this.paused = false;
+    this.setFinishDate(this.mins, this.segs);
+    this.counterTimer$ = this.start().subscribe((_) => {
+      if (this.time.minutes === 0 && this.time.seconds === 0) {
+        this.runnig = false;
+        if (this.main) {
+          this.presentAlert('TURNO 0', 'fail');
+        } else {
+          this.presentAlert('Fin de turno', 'ring');
+        }
+        this.counterTimer$.unsubscribe();
+      }
+      if (this.time.minutes === 1 && this.time.seconds === 0) {
+        playAlert('minute');
       }
     });
+  }
+
+  pause() {
+    this.paused = true;
+    this.runnig = false;
+    this.counterTimer$.unsubscribe();
+    this.mins = this.time.minutes;
+    //adicion de un segundo para cuadrar con tiempo. Porq falta un seg?
+    this.segs = this.time.seconds + 1;
+  }
+
+  stop() {
+    this.runnig = false;
+    this.paused = false;
+    this.counterTimer$.unsubscribe();
+    this.setFinishDate(this.gameTime, 0);
+    this.updateTime();
+  }
+
+  async presentAlert(mensaje: string, sonido: string) {
+    const alert = await this.alertController.create({
+      message: mensaje,
+      buttons: ['OK'],
+    });
+    playAlert(sonido);
+    await alert.present();
   }
 }
